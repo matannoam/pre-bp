83c0e342162e0d120a6176d46c890e41b3e04c85
Matan Noam Shavit <matan@lot18.com>
2016-12-29 16:08:13 -0500

    lower import size with new version of history. workaround needed to configure store before importing preact in any components

--- src/data/libraries.js
+++ src/data/libraries.js
@@ -2,7 +2,7 @@ const libraries = [
     {name: 'preact', version: '7.1.0'},
     {name: 'redux', version: '3.6.0'},
     {name: 'preact-redux', version: '1.2.0'},
-    {name: 'history', version: '3.2.1'},
+    {name: 'history', version: '4.5.0'},
     {name: 'preact-router', version: '2.3.2'},
     {name: 'react-router-redux', version: '4.0.7'},
 ]
--- src/index.js
+++ src/index.js
@@ -1,28 +1,31 @@
 import { h, render } from 'preact'
 import { Provider } from 'preact-redux'
 
-import { Router, Route } from 'preact-router'
 import { syncHistoryWithStore } from 'react-router-redux'
 import browserHistory from './lib/browserHistory'
 
 import configureStore from './stores/configureStore'
 import * as actions from './actions'
-import App from './containers/App'
-import MainPage from './containers/MainPage'
-import LibrariesPage from './containers/LibrariesPage'
 import libraries from './data/libraries'
 
+
 const store = configureStore()
+const history = syncHistoryWithStore(browserHistory, store)
+
 store.dispatch(actions.setLibraries(libraries))
 
-const history = syncHistoryWithStore(browserHistory, store)
+const { Router, Route } = require('preact-router')
+
+const App = require('./containers/App').default
+const MainPage = require('./containers/MainPage').default
+const LibrariesPage = require('./containers/LibrariesPage').default
 
 render(
   <Provider store={store}>
     <App>
-      <Router history={history}>
-        <Route path="/" component={MainPage} default />
-        <Route path="/libraries" component={LibrariesPage} />
+      <Router history={browserHistory}>
+        <Route path="/" component={MainPage} default/>
+        <Route path="/libraries" component={LibrariesPage}/>
       </Router>
     </App>
   </Provider>,
