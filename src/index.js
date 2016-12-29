fe07d5a7b7a0513c8fc0e5cd04f270dbf4615cda
Matan Noam Shavit <matan@lot18.com>
2016-12-29 17:27:11 -0500

    remove the need to require by configuring in functino file

--- src/index.js
+++ src/index.js
@@ -4,21 +4,17 @@ import { Provider } from 'preact-redux'
 import { syncHistoryWithStore } from 'react-router-redux'
 import browserHistory from './lib/browserHistory'
 
-import configureStore from './stores/configureStore'
+import { store, history } from './stores/configureStore'
 import * as actions from './actions'
 import libraries from './data/libraries'
 
+import { Router, Route } from 'preact-router'
 
-const store = configureStore()
-const history = syncHistoryWithStore(browserHistory, store)
+import App from './containers/App'
+import MainPage from './containers/MainPage'
+import LibrariesPage from './containers/LibrariesPage'
 
 store.dispatch(actions.setLibraries(libraries))
-
-const { Router, Route } = require('preact-router')
-
-const App = require('./containers/App').default
-const MainPage = require('./containers/MainPage').default
-const LibrariesPage = require('./containers/LibrariesPage').default
 
 render(
   <Provider store={store}>
