8e7c0d6db8ac1df67b7f298265e468c8a6a74b39
Matan Noam Shavit <matannoam@gmail.com>
2017-01-27 22:01:33 -0500

    show pre-bp version, update router version data

--- src/containers/App.js
+++ src/containers/App.js
@@ -6,7 +6,7 @@ import appStyle from '../styles/appStyle'
 export default function App ({ children }) {
   return (
     <div style={appStyle}>
-      <h1>pre-bp</h1>
+      <h1>pre-bp v0.4.1</h1>
       <h2>very small frontend boilerplate</h2>
       {children}
       <p><small>
